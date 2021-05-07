e28a79098b2642c44cd7ff96dce52a9b429ce082
twinkle-2007 <83075105+twinkle-2007@users.noreply.github.com>
2021-05-07 15:21:05 +0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var car,wall;
+var speed,weight;
+var deformation;
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(55,90);
+  weight=random(400,1500);
+  car = createSprite(200, 350, 50, 80);
+  wall=createSprite(1200,350,60,height/2);
+  wall.shapeColor=("cyan");
+  car.velocityX=speed;
+
+}
+
+function draw() {
+  background("white");  
+ 
+
+if(wall.x-car.x<(car.width+wall.width)/2){
+   car.velocityX=0;
+   var deformation=0.5 * speed* speed/22509;
+      if(deformation>180);
+      {
+      car.shapeColor=color(255,0,0);
+      }
+      if (deformation<180 && deformation>100){
+      car.shapeColor=color(230,230,0);
+      }
+      if (deformation<100){
+      car.shapeColor=color(0,255,0);
+      }
+      
+      
+}  
+
+  drawSprites();
+}
